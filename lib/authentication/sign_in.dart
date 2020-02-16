f5fe904a058e2c066bec3b6766f888c80e700fb3
Judy Kao <imjudykao@gmail.com>
2020-02-16 22:25:58 +0800

    add login page but hasn't deal with the firebase issue

--- lib/authentication/sign_in.dart
+++ lib/authentication/sign_in.dart
@@ -1,6 +1,6 @@
+import 'package:bnerd/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:bnerd/widgets/FormCard.dart';
 import 'package:bnerd/shared/load.dart';
 import 'package:bnerd/services/auth.dart';
 
@@ -106,7 +106,80 @@ class _SigninState extends State<Signin> {
                   SizedBox(
                     height: ScreenUtil.getInstance().setHeight(180),
                   ),
-                  FormCard(),
+                  Container(
+                    width: double.infinity,
+                    height: ScreenUtil.getInstance().setHeight(500),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0.0, 15.0),
+                              blurRadius: 15.0),
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0.0, -10.0),
+                              blurRadius: 10.0),
+                        ]),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Login",
+                              style: TextStyle(
+                                  fontSize: ScreenUtil.getInstance().setSp(45),
+                                  fontFamily: "Poppins-Bold",
+                                  letterSpacing: .6)),
+                          SizedBox(
+                            height: ScreenUtil.getInstance().setHeight(30),
+                          ),
+                          Text("Username",
+                              style: TextStyle(
+                                  fontFamily: "Poppins-Medium",
+                                  fontSize: ScreenUtil.getInstance().setSp(26))),
+                          TextFormField(
+                            decoration: textInputDecoration.copyWith(hintText: 'Email', hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)),
+                            validator: (val) =>val.isEmpty ? 'Enter an email' : null,
+                            onChanged: (val) {
+                              setState(() => email = val);
+                            },
+                          ),
+                          SizedBox(
+                            height: ScreenUtil.getInstance().setHeight(30),
+                          ),
+                          Text("PassWord",
+                              style: TextStyle(
+                                  fontFamily: "Poppins-Medium",
+                                  fontSize: ScreenUtil.getInstance().setSp(26))),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: textInputDecoration.copyWith(hintText: 'Password', hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)),
+                            validator: (val) =>val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                            onChanged: (val) {
+                              setState(() => password = val);
+                            },
+                          ),
+                          SizedBox(
+                            height: ScreenUtil.getInstance().setHeight(35),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontFamily: "Poppins-Medium",
+                                    fontSize: ScreenUtil.getInstance().setSp(28)),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                   SizedBox(height: ScreenUtil.getInstance().setHeight(40)),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
