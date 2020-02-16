867b534bf3b3f8eb7af9047c555c57f474cce657
Judy Kao <imjudykao@gmail.com>
2020-02-16 21:14:10 +0800

    add login page but hasn't deal with the firebase issue

---
+++ lib/pages/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstLoading extends StatefulWidget {
+  @override
+  _FirstLoadingState createState() => _FirstLoadingState();
+}
+
+class _FirstLoadingState extends State<FirstLoading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
