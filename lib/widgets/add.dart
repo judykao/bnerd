a6b2c7edc7807e27b1a03d405083c1c09e39e96a
Judy Kao <imjudykao@gmail.com>
2020-02-23 13:23:28 +0800

    Error is still there Finish Add()

--- lib/widgets/add.dart
+++ lib/widgets/add.dart
@@ -14,12 +14,11 @@ class _AddState extends State<Add> {
 
   final _formKey = GlobalKey<FormState> ();
   final List<String> numbers = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40',];
-  /*
+
   //form values
-  String _currentName;
-  String _currentCla;
-  String _currentNumber;
-   */
+  String _currentSubject;
+  String _currentContent;
+  //String _currentNumber;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +45,8 @@ class _AddState extends State<Add> {
         });
     }
 
+    String dropdownType = 'Homework';
+
     return StreamBuilder<HomeworkData>(
         stream: HomeworkDataBaseService(uid: homework.uid).homeworkData,
         builder: (context, snapshot) {
@@ -65,30 +66,58 @@ class _AddState extends State<Add> {
                   ListTile(
                     leading: Icon(Icons.access_time),
                     title: Text('$selectedDate'),
-                      onTap: () =>
-                      {
-                        _selectDate(context),
-
+                      onTap: () => _selectDate(context),
                         //TODO: 寫完讓widget 跑出來但還沒有取值
-                      }
                   ),
                   ListTile(
                     leading: new Icon(Icons.merge_type),
                     title: new Text('Type'),
-                    onTap: () =>
-                    {
+                    onTap: () => {
+                      DropdownButton<String>(
+                        value: dropdownType,
+                        icon: Icon(Icons.arrow_downward), iconSize: 24,
+                        elevation: 16,
+                        style: TextStyle(color: Colors.deepPurple), underline: Container(height: 2, color: Colors.deepPurpleAccent, ),
+                        onChanged: (String newValue) {
+                          setState(() {dropdownType = newValue;});},
+                        items: <String>['Homework', 'Test', 'Note'].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
                       //TODO:觸發下拉選單，可以設定類型
                     },
                   ),
-                  /*
-                  TextFormField(
-                    initialValue: homeworkData.content,
-                    decoration: textInputDecoration.copyWith(hintText: 'NUmber'),
-                    validator: (val) => val.isEmpty ? 'Please enter your number' : null,
-                    onChanged: (val) => setState(() => _currentNumber = val),
+                  Row(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.book),
+                        title: Text('Subject'),
+                      ),
+                      TextFormField(
+                        initialValue: homeworkData.content,
+                        decoration: textInputDecoration.copyWith(),
+                        validator: (val) => val.isEmpty ? 'Please enter a subject' : null,
+                        onChanged: (val) => setState(() => _currentSubject = val),
+                      ),
+                    ],
                   ),
-
-                   */
+                  Row(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.subject),
+                        title: Text('Content'),
+                      ),
+                      TextFormField(
+                        initialValue: homeworkData.content,
+                        decoration: textInputDecoration.copyWith(),
+                        validator: (val) => val.isEmpty ? 'Please enter a subject' : null,
+                        onChanged: (val) => setState(() => _currentContent = val),
+                      ),
+                    ],
+                  ),
                   RaisedButton(
                     color: Colors.blueGrey[700],
                     child: Text(
@@ -97,7 +126,7 @@ class _AddState extends State<Add> {
                     ),
                     onPressed: () async {
                       if(_formKey.currentState.validate()) {
-                        await HomeworkDataBaseService().createHomeworkData('$selectedDate', 'subject', 'type', 'content', 'note');
+                        await HomeworkDataBaseService().createHomeworkData('$selectedDate', '$_currentSubject', '$dropdownType', '$_currentContent', 'note');
                         Navigator.pop(context);
                       }
                     },
